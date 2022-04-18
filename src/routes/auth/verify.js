476320ab210890ee0acb05e16a430a3a1df5595e
Mbrick2 <90691415+Mbrick2@users.noreply.github.com>
2022-04-18 21:13:15 +1000

    Update verify.js

--- src/routes/auth/verify.js
+++ src/routes/auth/verify.js
@@ -5,7 +5,7 @@ import { createSession, getUserByUsername, createUser } from '$lib/db';
 export async function post({ request }) {
   const { privateCode } = await request.json();
 
-  const res = await fetch('https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode=' + privateCode);
+  const res = await fetch('https://auth.itinerary.eu.org/api/auth/verifyToken?privateCode=' + privateCode = "&name=Aviate");
   const json = await res.json();
 
   if (!json.valid) return {
