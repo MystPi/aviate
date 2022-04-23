b16850458d18fc661622b830b82600d8a97e8805
MystPi <86574651+MystPi@users.noreply.github.com>
2022-04-23 13:18:29 -0400

    feat: Google Analytics

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -32,6 +32,15 @@
 
 <svelte:head>
   <script src="https://unpkg.com/feedbackfin@^1" defer></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4XEN5K9MEQ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-4XEN5K9MEQ');
+  </script>
 </svelte:head>
 
 <img src="/blur.svg" alt="" class="blob" id="right">
