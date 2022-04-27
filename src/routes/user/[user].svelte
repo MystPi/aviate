0b9bd0b5f54ca4c29610b6095a158abb305f1612
MystPi <86574651+MystPi@users.noreply.github.com>
2022-04-27 17:52:39 -0400

    feat: user page

---
+++ src/routes/user/[user].svelte
@@ -0,0 +1,30 @@
+<script context="module">
+  export async function load({ params, fetch }) {
+    const status = await fetch('/api/' + params.user).then(res => res.json());
+
+    return {
+      props: {
+        status: status.status,
+        user: params.user
+      }
+    };
+  }
+</script>
+
+<script>
+  import Header from '$lib/components/Header.svelte';
+
+  export let status;
+  export let user;
+</script>
+
+<svelte:head>
+  <title>View a user's status - Aviate</title>
+</svelte:head>
+
+<Header>
+  <span slot="title">View <span class="highlight">{user}'s</span> Status</span>
+  <span slot="subtitle">Set your own status on your <a href="/dashboard">dashboard</a>.</span>
+</Header>
+
+<input class="input is-family-monospace" value={status} readonly/>
