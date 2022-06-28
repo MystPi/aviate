6666575a4348dcb2d19b4e681d169491a5b85e30
MystPi <86574651+MystPi@users.noreply.github.com>
2022-06-28 17:14:32 -0400

    fix: escape `ilike` pattern

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createClient } from '@supabase/supabase-js';
 
-
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_SECRET_KEY
 );
 
-
 async function getData(table, type, gotten) {
   return await supabase.from(table).select().eq(type, gotten);
 }
-
 
 export async function getUserByUsername(username, sensitive = true) {
   let user;
@@ -19,14 +16,16 @@ export async function getUserByUsername(username, sensitive = true) {
   if (sensitive) {
     user = await getData('users', 'username', username);
   } else {
-    user = await supabase.from('users').select().ilike('username', username);
+    user = await supabase
+      .from('users')
+      .select()
+      .ilike('username', username.replace(/_|%/g, '$1$1'));
   }
 
   if (!user.data?.length) return Promise.resolve(null);
 
   return Promise.resolve(user.data[0]);
 }
-
 
 export async function getUserFromCookies(cookies) {
   if (cookies.session_id) {
@@ -40,16 +39,15 @@ export async function getUserFromCookies(cookies) {
   return Promise.resolve(null);
 }
 
-
 export async function createUser(user) {
   const existing = await getData('users', 'username', user.username);
 
-  if (existing.data?.length) return Promise.reject(new Error('User already exists'));
+  if (existing.data?.length)
+    return Promise.reject(new Error('User already exists'));
 
   await supabase.from('users').insert([user]);
   return Promise.resolve(user);
 }
-
 
 export async function createSession(username) {
   const existing = await getData('sessions', 'username', username);
@@ -59,12 +57,11 @@ export async function createSession(username) {
   const session = {
     id: uuidv4(),
     username,
-  }
+  };
 
   await supabase.from('sessions').insert([session]);
   return Promise.resolve(session);
 }
-
 
 export async function getSession(sessionId) {
   const session = await getData('sessions', 'id', sessionId);
@@ -74,7 +71,6 @@ export async function getSession(sessionId) {
   return Promise.resolve(session.data[0]);
 }
 
-
 export async function deleteSession(sessionId) {
   const session = await getData('sessions', 'id', sessionId);
 
@@ -83,7 +79,6 @@ export async function deleteSession(sessionId) {
   await supabase.from('sessions').delete().eq('id', sessionId);
   return Promise.resolve(sessionId);
 }
-
 
 export async function setStatus(username, status) {
   const user = await getUserByUsername(username);
@@ -95,24 +90,20 @@ export async function setStatus(username, status) {
   return Promise.resolve(user);
 }
 
-
 export async function getAllUsers() {
   const users = await supabase.from('users').select();
   return Promise.resolve(users.data || []);
 }
-
 
 export async function sendFeedback(feedback) {
   await supabase.from('feedback').insert([feedback]);
   return Promise.resolve(feedback);
 }
 
-
 export async function getAllFeedback() {
   const feedback = await supabase.from('feedback').select();
   return Promise.resolve(feedback.data || []);
 }
-
 
 export async function deleteFeedback(ts) {
   const feedback = await getData('feedback', 'timestamp', ts);
