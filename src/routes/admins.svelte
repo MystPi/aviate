80b3382b448021e10c4fc523d7409117a34f74f0
MystPi <86574651+MystPi@users.noreply.github.com>
2022-07-06 08:17:08 -0400

    chore: update admins list

--- src/routes/admins.svelte
+++ src/routes/admins.svelte
@@ -13,9 +13,9 @@
 
 <div class="content">
   <ul>
-    <li>NFlex23 - <a href="https://scratch.mit.edu/users/NFlex23#comments">Scratch</a></li>
+    <li>MaterArc (head admin) - <a href="https://scratch.mit.edu/users/MaterArc#comments">Scratch</a></li>
+    <li>NFlex23 (owner) - <a href="https://scratch.mit.edu/users/NFlex23#comments">Scratch</a></li>
     <li>Adzboy - <a href="https://scratch.mit.edu/users/Adzboy#comments">Scratch</a></li>
-    <li>MaterArc - <a href="https://scratch.mit.edu/users/MaterArc#comments">Scratch</a></li>
     <li>Chiroyce - <a href="https://scratch.mit.edu/users/Chiroyce#comments">Scratch</a></li>
     <li>scratchusername40 - <a href="https://scratch.mit.edu/users/scratchusername40#comments">Scratch</a></li>
     <li>ScolderCreations - <a href="https://scratch.mit.edu/users/ScolderCreations#comments">Scratch</a></li>
