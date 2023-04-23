506c2b62adf9743a3c7d3ce4b40ac509b146eb99
MystPi <86574651+MystPi@users.noreply.github.com>
2023-04-23 14:06:56 -0400

    🚸 Add pagetitle to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
   import { PageHeader } from '$lib/components';
 </script>
 
-<PageHeader>
+<PageHeader pagetitle={$page.error?.message}>
   <span slot="title">{$page.status}</span>
   <span slot="subtitle">{$page.error?.message} </span>
 </PageHeader>
