b618400fc5216f3e49f551e9dfcf4c6bfcfa4321
MystPi <86574651+MystPi@users.noreply.github.com>
2023-04-24 15:33:32 -0400

    🩹 Add Seo component to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import { PageHeader, Seo } from '$lib/components';
   import { page } from '$app/stores';
-  import { PageHeader } from '$lib/components';
 </script>
 
-<PageHeader pagetitle={$page.error?.message}>
+<Seo title={$page.error?.message} />
+
+<PageHeader>
   <span slot="title">{$page.status}</span>
   <span slot="subtitle">{$page.error?.message} </span>
 </PageHeader>
