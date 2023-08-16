4caa3870bfe684eaf26c7aa56a40534acc645943
MystPi <86574651+MystPi@users.noreply.github.com>
2023-08-16 15:45:05 -0400

    💬 Add bit about emojis being unsupported

--- src/lib/components/DynamicImage.svelte
+++ src/lib/components/DynamicImage.svelte
@@ -36,7 +36,8 @@
     Aviate now has <em>dynamic images</em>. They let you embed your status into many websites in the
     form of an image. The width and height may take some fiddling to get just right, but in the end
     you'll have a dynamic image that can be used on your GitHub profile or anywhere else that
-    supports images from external sites.
+    supports images from external sites. Emojis are sadly unsupported at the moment, and will display as
+    a black shape.
   </p>
 
   <hr class="border-slate-200" />
